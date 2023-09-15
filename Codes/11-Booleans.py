5fed1c751e0af2912c6a1d6f209bcc56cd0b9ef2
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-15 13:14:35 +0330

    List Boolean Operators

---
+++ Codes/11-Booleans.py
@@ -0,0 +1,14 @@
+"""
+Boolean
+"""
+
+like_coffee = True
+like_tea = False
+
+favorite_food = "Pizza"
+favorite_number = 32
+
+print(like_coffee)
+print(type(like_coffee))
+print(type(favorite_food))
+print(type(favorite_number))
---
+++ Codes/12-Operators.py
@@ -0,0 +1,22 @@
+"""
+Operators
+"""
+# Comparison Operators
+# Equals
+print(1 == 2)
+
+# Not Equals
+print(1 != 2)
+
+print(1 > 2)
+print(1 < 2)
+print( 1 >= 1)
+print(1 <= 2)
+
+# Logical Operators
+print(1 > 3 and 5 < 7)
+
+print(1 > 3 or 5 < 7)
+
+print(1 == 1)
+print(not(1 == 1))
