72f14bf3713c28d0859dce7cc744e0347545d777
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-15 15:02:00 +0330

    Imports

---
+++ Codes/standardlib.py
@@ -0,0 +1,20 @@
+"""
+Standard Library comes with Python
+
+Useful methods!
+"""
+
+# Random
+
+import random
+
+types_of_drink = ['Soda', 'Coffee', 'Water', 'Tea']
+print(random.choice(types_of_drink))
+
+print(random.randint(1, 10))
+
+
+import math
+
+square_root = math.sqrt(64)
+print(square_root)
