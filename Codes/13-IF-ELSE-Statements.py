87765938f8d46158e2c78b14e119aeb99079f376
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-15 13:22:29 +0330

    Update 13-IF-ELSE-Statements.py

---
+++ Codes/13-IF-ELSE-Statements.py
@@ -0,0 +1,67 @@
+"""
+Flow Controls: If Else ElIf
+"""
+
+x = 1
+
+if x == 1 :
+    print("X is 1")
+
+print("Outside of if statement")
+
+##############################################
+
+x = 2
+
+if x == 1 :
+    print("X is 1")
+
+print("Outside of if statement")
+
+##############################################
+
+x = 2
+
+if x > 1 :
+    print("X is greater than 1")
+else :
+    print("x is not greater than 1")
+
+print("Outside of if statement")
+
+##############################################
+
+hour = 13
+
+if hour < 15 :
+    print("Good morning!")
+else :
+    print("Good Afternoon!")
+
+print("Outside of if statement")
+
+##############################################
+
+hour = 16
+
+if hour < 15 :
+    print("Good morning!")
+else :
+    print("Good Afternoon!")
+
+print("Outside of if statement")
+
+##############################################
+
+hour = 13
+
+if hour < 15 :
+    print("Good morning!")
+elif hour < 20 :
+    print("Good Afternoon")
+else :
+    print("Good Night!")
+
+print("Outside of if statement")
+
+##############################################
